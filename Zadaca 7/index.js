5f27a4cf1e130fbf137f831769280c350635dc60
janevsky <baze.janevski@gmail.com>
2020-11-25 17:05:54 +0100

    first commit

---
+++ Zadaca 7/index.js
@@ -0,0 +1,17 @@
+// 1. Write a function to find first and last digit of a number.
+
+// 2. Write a function to reverse the following array (manually and  using .reverse array method )
+
+// input array = [10, 20, 30, 40, 50]
+
+// output array = [50, 40, 30, 20 ,10]
+
+// 3. Write a functions to calculate the perimeter and the area of a square
+
+// 4. Write a functions to calculate the perimeter and the area of a rectangle
+
+// 5. Write a functions to calculate the perimeter and the area of a circle
+
+// 6. Write a function to return the last n elements from a given array (manually and  using .reverse array method)
+
+// ** n and the array should be parameters in the function
